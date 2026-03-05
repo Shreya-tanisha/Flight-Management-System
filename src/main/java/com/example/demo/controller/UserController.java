fb2d5cd245289d057f5c70a8f902de0a5ad1c515
Shreya <oturkarshreya@gmail.com>
2026-03-05 12:36:53 +0530

    Restored project structure from development branch

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.entity.User;
+import com.example.demo.requestdto.UserRequestdto;
+import com.example.demo.responsedto.UserResponsedto;
+import com.example.demo.service.UserService;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+	@Autowired
+    UserService userService;
+
+    // 🔹 Save User
+    @PostMapping("/save")
+    public User save(@RequestBody UserRequestdto dto) {
+        return userService.save(dto);
+    }
+
+    // 🔹 Get User By ID
+    @GetMapping("/{id}")
+    public UserResponsedto getById(@PathVariable UUID id) {
+        return userService.getById(id);
+    }
+
+    // 🔹 Get All Users
+    @GetMapping("/all")
+    public List<UserResponsedto> findAllUser() {
+        return userService.findAllUser();
+    }
+
+    // 🔹 Update User
+    @PutMapping("/{id}")
+    public UserResponsedto update(@PathVariable UUID id,
+                                  @RequestBody UserRequestdto dto) {
+        return userService.update(id, dto);
+    }
+
+    // 🔹 Delete User
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return "User deleted successfully";
+    }
+}
---
+++ src/main/java/com/example/demo/requestdto/UserRequestdto.java
@@ -0,0 +1,11 @@
+package com.example.demo.requestdto;
+
+import lombok.Data;
+
+@Data
+public class UserRequestdto {
+private String userName;
+private String UserAddress;
+private Integer userAge;
+private String gender;
+}
