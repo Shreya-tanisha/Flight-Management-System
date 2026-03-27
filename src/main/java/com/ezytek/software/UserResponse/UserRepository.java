660d916033a68ab04f5154631f0d7cdc8e0895ff
Shreya <oturkarshreya@gmail.com>
2026-03-27 11:07:57 +0530

    Merge pull request #4 from Shreya-tanisha/code-standardisation
    Code standardisation

--- src/main/java/com/ezytek/software/UserResponse/UserRepository.java
+++ src/main/java/com/ezytek/software/UserResponse/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import com.ezytek.software.entity.User;
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,UUID> {
 
 }
--- src/main/java/com/ezytek/software/requestdto/BookingRequestDto.java
+++ src/main/java/com/ezytek/software/requestdto/BookingRequestDto.java
@@ -3,11 +3,12 @@ package com.ezytek.software.requestdto;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
 public class BookingRequestDto {
     private Long flightId;
-    private Long userId;
+    private UUID userId;
 
     private String seatNumber;
     private String seatClass;
